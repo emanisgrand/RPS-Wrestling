6e9e335dd2dc19207abda87f56c47887f4dea809
eman <tenhitcombo@gmail.com>
2019-07-24 21:07:52 -0400

    00-gameplayController.cs notes

--- Assets/e-Assets/Scripts/GameplayController.cs
+++ Assets/e-Assets/Scripts/GameplayController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum GameChoices { 
+public enum GameChoices {   
+    // * I was thinking something like this, but more for the outcomes than the choices themselves. I will have to try it this way though since it's basically done here.
     NONE,
     ROCK,
     PAPER,
@@ -12,7 +13,12 @@ public enum GameChoices {
 
 public class GameplayController : MonoBehaviour {
 
-    [SerializeField]
+    [SerializeField]  //? none of the sprite image member variables will be necessary unless 
+    //? I decide to implement some UI thing with them later. Which I am actually thinking 
+    //? about doing. Something like the Mix&Jam Persona 4 
+    //? cutscenes where they make a really big deal about the choice you end up making is 
+    //? actually quite perfect. 
+     
     private Sprite rock_Sprite, paper_Sprite, scissors_Sprite;
 
     [SerializeField]
@@ -29,12 +35,15 @@ public class GameplayController : MonoBehaviour {
         animationController = GetComponent<AnimationController>();
     }
 
+    //* Idk, but maybe this is where choices are set. */
     public void SetChoices(GameChoices gameChoices) { 
     
         switch(gameChoices) {
 
             case GameChoices.ROCK:
 
+            /* //! In this case, an image is switched out to represent the player's
+            ! choice.  */
                 playerChoice_Img.sprite = rock_Sprite;
 
                 player_Choice = GameChoices.ROCK;
@@ -68,10 +77,14 @@ public class GameplayController : MonoBehaviour {
     void SetOpponentChoice() {
 
         int rnd = Random.Range(0, 3);
+        //! switch cases used AGAIN! Except here there is not an enums array and instead
+        //! choices are created in the method itself and defined in the switcher.
 
         switch(rnd) {
 
             case 0:
+            //? is this redundant? No. Because this is opponent choice and so is separate from
+            //? player choice so it must be distinct, right? 
 
                 opponent_Choice = GameChoices.ROCK;
 
@@ -102,13 +115,15 @@ public class GameplayController : MonoBehaviour {
     }
 
     void DetermineWinner() { 
-    
+        //! here is the actual Decision tree, right? 
+
         if(player_Choice == opponent_Choice) {
             // draw
 
             infoText.text = "It's a Draw!";
-            StartCoroutine(DisplayWinnerAndRestart());
-
+            StartCoroutine(DisplayWinnerAndRestart());  //! This "DisplayWinnerAndRestart()" function is
+                                                        //! I think, where I would be executing the 
+                                                        //! animations.
             return;
 
         }
@@ -169,6 +184,7 @@ public class GameplayController : MonoBehaviour {
 
     }
 
+    //! If I want to make this guy wait, then I need to use IEnumerator. 
     IEnumerator DisplayWinnerAndRestart() {
         yield return new WaitForSeconds(2f);
 
@@ -177,7 +193,8 @@ public class GameplayController : MonoBehaviour {
         yield return new WaitForSeconds(2f);
 
         infoText.gameObject.SetActive(false);
-
+        //? My animations will be way more complicated and varied than this.
+        //? It would be nice to have an array of animations to sift through 
         animationController.ResetAnimations();
 
     }
