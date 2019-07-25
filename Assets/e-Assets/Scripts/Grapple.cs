00e6e285faf53cb0c698d0b8fea3d903f3b5eed4
eman <tenhitcombo@gmail.com>
2019-07-24 21:43:50 -0400

    pretty substandard code to start with, but we're getting somewhere & there's notes.

---
+++ Assets/e-Assets/Scripts/Grapple.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+    // private serialized fields
+    [SerializeField] private    float       reach     = 5f;
+    [SerializeField] private    float       winDamage = 1f;
+    
+    // private member variables
+    private                     RaycastHit  hit;
+    private                     Opponent    opponent;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {  //considering fine tuning options here with GetKeyUp */
+            // make the ray come out of the hand. 
+            // OR, even better
+            //TODO: Trigger an animation, and change this logic to collision detection to see
+            //! if the collider from the animation is hitting the opponent. 
+            Debug.DrawRay(transform.position, transform.forward * reach, Color.yellow);
+            Grab();
+        }
+    }
+
+    void Grab(){
+        //TODO: implement a layer mask for turnbuckle interaction
+        if (Physics.Raycast(transform.position, transform.forward, out hit, reach)){
+
+        if (hit.collider.tag == "Opponent") {
+               opponent = hit.collider.gameObject.GetComponent<Opponent>();
+               opponent.TakeDamage(winDamage);
+            }    
+        }
+    }
+}
---
+++ Assets/e-Assets/Scripts/Opponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opponent : MonoBehaviour
+{
+    [SerializeField] private float health = 3f;
+
+    public void TakeDamage(float damageAmount){
+        health -= damageAmount;
+
+        if (health <= 0) {
+            OpponentLoses();
+        }
+    }
+
+    void OpponentLoses(){
+        // Player can do a pin animation
+        // Opponent is counted out
+        print("Opponent has lost the match");
+
+    }
+}
